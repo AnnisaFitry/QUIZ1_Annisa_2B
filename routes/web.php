a2aa83b4dafc2d068850a0892527390dc63dc550
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-03-21 14:37:34 +0700

    route_controller

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\JonController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/products', function () {
+    return view('products');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', [JonController::class, 'Contact']);
